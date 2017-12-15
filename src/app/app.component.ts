262abf1ad33d9146cb3281dbe62f4c3134669df7
Angular CLI <angular-cli@angular.io>
2017-12-15 20:18:28 +0530

    chore: initial commit from @angular/cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mmd-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'mmd';
+}
